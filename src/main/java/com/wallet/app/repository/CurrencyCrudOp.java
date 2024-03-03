f54f7357ddbd6c03d45188ce1e3c3ade6c20f6ea
HarenaFiantso <fiantsorav@gmail.com>
2024-03-03 14:06:31 +0300

    chore: implement findById currency

--- src/main/java/com/wallet/app/repository/CurrencyCrudOp.java
+++ src/main/java/com/wallet/app/repository/CurrencyCrudOp.java
@@ -1,5 +1,6 @@
 package com.wallet.app.repository;
 
+import com.wallet.app.repository.conf.DatabaseConnection;
 import com.wallet.app.repository.model.Currency;
 import org.springframework.stereotype.Repository;
 
@@ -11,9 +12,45 @@ import java.util.List;
 
 @Repository
 public class CurrencyCrudOp implements CrudOperations<Currency> {
+  private static final String CURRENCY_ID_COLUMN = "currency_id";
+  private static final String CURRENCY_NAME_COLUMN = "currency_name";
+  private static final String CURRENCY_CODE_COLUMN = "currency_code";
+
+  private static final String SELECT_BY_ID_QUERY = "SELECT * FROM currency WHERE currency_id = ?";
+  private static final String SELECT_ALL_QUERY = "SELECT * FROM currency";
+  private static final String INSERT_QUERY =
+      "INSERT INTO currency (currency_name, currency_code) VALUES (CAST(? AS currency_name), CAST(?"
+          + " AS currency_code)) RETURNING *";
+  private static final String UPDATE_QUERY =
+      "UPDATE currency SET currency_name = CAST(? AS currency_name), currency_code = CAST(? AS"
+          + " currency_code) WHERE currency_id = ? RETURNING *";
+  private static final String DELETE_QUERY = "DELETE FROM currency WHERE currency_id = ?";
+
   @Override
   public Currency findById(Currency toFind) {
-    return null;
+    Currency currency = null;
+    Connection connection = null;
+    PreparedStatement statement = null;
+    ResultSet resultSet = null;
+
+    try {
+      connection = DatabaseConnection.getConnection();
+
+      statement = connection.prepareStatement(SELECT_BY_ID_QUERY);
+      statement.setString(1, toFind.getCurrencyId());
+
+      resultSet = statement.executeQuery();
+
+      if (resultSet.next()) {
+        currency = new Currency();
+        currency.setCurrencyId(resultSet.getString(CURRENCY_ID_COLUMN));
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException("Failed to retrieve currency : " + e.getMessage());
+    } finally {
+      closeResources(connection, statement, resultSet);
+    }
+    return currency;
   }
 
   @Override
