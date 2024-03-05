caf486abdd6658127b5764af8ab3416429a4068c
HarenaFiantso <fiantsorav@gmail.com>
2024-03-05 23:14:50 +0300

    chore: implement find all categories repository

--- src/main/java/com/wallet/app/repository/CategoryRepository.java
+++ src/main/java/com/wallet/app/repository/CategoryRepository.java
@@ -1,17 +1,16 @@
 package com.wallet.app.repository;
 
 import com.wallet.app.db.DBConnection;
-import com.wallet.app.db.entity.Account;
 import com.wallet.app.db.entity.Category;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Repository;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class CategoryRepository implements CrudRepository<Category> {
@@ -20,10 +19,13 @@ public class CategoryRepository implements CrudRepository<Category> {
   private static final String CATEGORY_ID_COLUMN = "category_id";
   private static final String CATEGORY_NAME_COLUMN = "category_name";
 
+  private static final String SELECT_ALL_QUERY = "SELECT * FROM account";
   private static final String INSERT_QUERY =
       "INSERT INTO category (category_name) VALUES (?) RETURNING *";
-  private static final String UPDATE_QUERY = "UPDATE category SET category_name = ? WHERE category_id = ?";
+  private static final String UPDATE_QUERY =
+      "UPDATE category SET category_name = ? WHERE category_id = ?";
   private static final String DELETE_QUERY = "DELETE FROM category WHERE category_id = ?";
+
   @Override
   public Category findById(String toFind) {
     return null;
@@ -31,7 +33,31 @@ public class CategoryRepository implements CrudRepository<Category> {
 
   @Override
   public List<Category> findAll() {
-    return null;
+    List<Category> categories = new ArrayList<>();
+    Connection connection = null;
+    PreparedStatement statement = null;
+    ResultSet resultSet = null;
+
+    try {
+      connection = DBConnection.getConnection();
+
+      statement = connection.prepareStatement(SELECT_ALL_QUERY);
+      resultSet = statement.executeQuery();
+
+      while (resultSet.next()) {
+        Category category =
+            new Category(
+                resultSet.getString(CATEGORY_ID_COLUMN), resultSet.getString(CATEGORY_NAME_COLUMN));
+
+        categories.add(category);
+      }
+      logger.info("categories retrieved successfully ✅");
+    } catch (SQLException e) {
+      logger.error("Failed to retrieve categories ❌: {}", e.getMessage());
+    } finally {
+      closeResources(connection, statement, resultSet);
+    }
+    return categories;
   }
 
   @Override
@@ -50,8 +76,7 @@ public class CategoryRepository implements CrudRepository<Category> {
 
       if (resultSet.next()) {
         return new Category(
-            resultSet.getString(CATEGORY_ID_COLUMN),
-            resultSet.getString(CATEGORY_NAME_COLUMN));
+            resultSet.getString(CATEGORY_ID_COLUMN), resultSet.getString(CATEGORY_NAME_COLUMN));
       }
       logger.info("Category saved successfully ✅");
     } catch (SQLException e) {
@@ -79,8 +104,7 @@ public class CategoryRepository implements CrudRepository<Category> {
       if (resultSet.next()) {
         category =
             new Category(
-                resultSet.getString(CATEGORY_ID_COLUMN),
-                resultSet.getString(CATEGORY_NAME_COLUMN));
+                resultSet.getString(CATEGORY_ID_COLUMN), resultSet.getString(CATEGORY_NAME_COLUMN));
       }
 
       logger.info("category updated successfully ✅");
@@ -110,8 +134,7 @@ public class CategoryRepository implements CrudRepository<Category> {
       if (resultSet.next()) {
         category =
             new Category(
-                resultSet.getString(CATEGORY_ID_COLUMN),
-                resultSet.getString(CATEGORY_NAME_COLUMN));
+                resultSet.getString(CATEGORY_ID_COLUMN), resultSet.getString(CATEGORY_NAME_COLUMN));
       }
 
       logger.info("category deleted successfully ✅");
@@ -125,7 +148,8 @@ public class CategoryRepository implements CrudRepository<Category> {
   }
 
   @Override
-  public void closeResources(Connection connection, PreparedStatement statement, ResultSet resultSet) {
+  public void closeResources(
+      Connection connection, PreparedStatement statement, ResultSet resultSet) {
     try {
       if (resultSet != null) {
         resultSet.close();
