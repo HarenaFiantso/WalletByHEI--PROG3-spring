7444e05b85381db2b2f0870d0dfe69e1e8de2c61
HarenaFiantso <fiantsorav@gmail.com>
2024-03-06 11:31:12 +0300

    chore: implement transaction controller

---
+++ src/main/java/com/wallet/app/controller/TransactionController.java
@@ -0,0 +1,60 @@
+package com.wallet.app.controller;
+
+import com.wallet.app.db.entity.Category;
+import com.wallet.app.db.entity.Transaction;
+import com.wallet.app.service.TransactionService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/transactions")
+public class TransactionController {
+  private final TransactionService transactionService;
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Transaction> getTransactionById(@PathVariable String id) {
+    Transaction transaction = transactionService.retrieveTransactionById(id);
+    if (transaction != null) {
+      return ResponseEntity.ok(transaction);
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @GetMapping("/all")
+  public ResponseEntity<List<Transaction>> getAllTransactions() {
+    List<Transaction> transactions = transactionService.retrieveAllTransactions();
+    return ResponseEntity.ok(transactions);
+  }
+
+  @PostMapping("/create")
+  public ResponseEntity<Transaction> createCategory(@RequestBody Transaction toSave) {
+    Transaction savedTransaction = transactionService.createTransaction(toSave);
+
+    if (savedTransaction != null) {
+      return ResponseEntity.ok(savedTransaction);
+    } else {
+      return ResponseEntity.badRequest().build();
+    }
+  }
+
+  @PutMapping("/update/{id}")
+  public Transaction udpateTransaction(@PathVariable String id, @RequestBody Transaction toUpdate) {
+    toUpdate.setTransactionId(id);
+    return transactionService.updateTransaction(toUpdate);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Transaction> deleteTransaction(@PathVariable String id) {
+    Transaction deletedTransaction = transactionService.deleteTransactionById(id);
+    if (deletedTransaction != null) {
+      return ResponseEntity.ok(deletedTransaction);
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+}
